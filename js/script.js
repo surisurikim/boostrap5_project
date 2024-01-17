33371359070ae42d199993cd7abb36709824dc29
surisurikim <niceonesuri@gmail.com>
2024-01-17 16:25:44 +0900

    add Navigation & Header

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('navbar-sticky');
+      toTopBtn.classList.add('show');
+    } else {
+      navbar.classList.remove('navbar-sticky');
+      toTopBtn.classList.remove('show');
+    }
+  });
+}
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', userScroll);
